b1e42b5699733d75527fd9b10dce3d265fac4317
csparker247 <csparker247@gmail.com>
2013-05-11 13:54:02 -0400

    summarize.sh now checks multispectral data for inconsistencies

--- summarize.sh
+++ summarize.sh
@@ -9,6 +9,12 @@ echo Summarize - Preprocessing Report on Multispectral Data
 echo ------------------------------------------------------
 echo
 
+containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
 report="Summarize Report - $(date +"%F")_$(date +"%T").txt"
 
 echo >>$report
@@ -34,6 +40,12 @@ for i in */; do
 			  # If already found a flatfield, call a warning
 			  if [[ $flat_exists == "true" ]]; then
 			  	echo "     WARNING: Multiple flatfield folders detected for $(basename $i)!">>$report 
+			  fi
+			  # If first flatfield found, collect multispectral info
+			  if [[ $flat_exists != "true" ]]; then
+			  	for k in $(basename $i)/$(basename $j)/Processed/*.tif; do
+			  		eval "$(basename $i)+=( "$(exiv2 -qpa $k | grep Exif.Photo.SpectralSensitivity | awk '{print $4}' | sed 's/(\([0-9A-Za-z]*\)nm,/\1/')" )"
+			  	done
 			  fi
 			  flat_exists="true"
 		  else
@@ -82,6 +94,65 @@ echo "     Volume Name: $i">>$report
     LIST=$(for j in $LIST; do echo "$j "; done | sort | tr -d '\n')
     echo "          $LIST">>$report
 echo >>$report
+done
+
+echo >>$report
+echo "-----------------------------">>$report
+echo "  Multispectral Information">>$report
+echo "-----------------------------">>$report
+MASTER=( 365 450 465 505 535 592 625 638 700 730 780 850 940 non )
+
+for i in */; do
+# List values found in Flats folder
+TEMP="\${$(basename $i)[@]}"  
+LIST=`eval echo $TEMP`
+LIST=$(for k in $LIST; do echo "$k "; done | sort | tr -d '\n')
+
+echo >>$report
+echo "     Flatfield wavelengths for $(basename $i):">>$report
+echo "          $LIST">>$report
+
+	for j in $(basename $i)/*; do
+	echo >>$report
+		if [[ "$(basename $j)" != FLATS_* ]]; then
+		if [[ -d $(basename $i)/$(basename $j)/Processed ]]; then
+		for k in $(basename $i)/$(basename $j)/Processed/*.tif; do
+			test_array+=("$(exiv2 -qpa $k | grep Exif.Photo.SpectralSensitivity | awk '{print $4}' | sed 's/(\([0-9A-Za-z]*\)nm,/\1/')")
+			test_array=( $(for m in ${test_array[@]}; do echo "$m "; done | sort | tr -d '\n') )
+		done
+		
+		echo "     Wavelengths for $(basename $j):">>$report
+		echo "          ${test_array[@]}">>$report
+		echo >>$report
+		
+		for k in ${MASTER[@]}; do
+			containsElement "$k" "${test_array[@]}"
+			if [[ $? == "1" ]]; then
+				echo "     WARNING: $(basename $j) in daily folder $(basename $i) is missing Standard Eureka Vision exposure ${k}nm">>$report
+			fi
+		done
+		
+		for k in $LIST; do
+			containsElement "$k" "${test_array[@]}"
+			if [[ $? == "1" ]]; then
+				echo "     WARNING: $(basename $j) in daily folder $(basename $i) is missing exposure ${k}nm found in the flatfields folder">>$report
+			fi
+		done
+		
+		for k in ${test_array[@]}; do
+			containsElement "$k" $LIST
+			if [[ $? == "1" ]]; then
+				echo "     WARNING: $(basename $j) in daily folder $(basename $i) is missing a corresponding flatfield for exposure ${k}nm">>$report
+			fi		
+		done
+		
+		unset test_array
+		else
+			echo "     WARNING: Processed folder not found in $j">>$report
+			echo >>$report
+		fi
+		fi	
+	done
 done
 
 cat "$report" | grep "WARNING"
