69de8038bcf6e41aedf4061b952670cdc22f8972
csparker247 <csparker247@gmail.com>
2013-05-13 10:35:25 -0400

    Documentation update and added some type checks to summarize.sh

--- summarize.sh
+++ summarize.sh
@@ -9,19 +9,21 @@ echo Summarize - Preprocessing Report on Multispectral Data
 echo ------------------------------------------------------
 echo
 
+# Function for comparing two arrays
 containsElement () {
   local e
   for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
   return 1
 }
 
+# Generate report name and setup report
 report="Summarize Report - $(date +"%F")_$(date +"%T").txt"
 
 echo >>$report
 echo "Summarize Report - $PWD - $(date +"%F") - $(date +"%T")">>$report
 echo >>$report
 
-## Collect Volume names and page numbers
+# Collect Volume names and page numbers
 echo "---------------------------">>$report
 echo "Folder Structure and Naming">>$report
 echo "---------------------------">>$report
@@ -32,8 +34,9 @@ vol_exists=""
 for i in */; do
 	echo "Daily folder: $(basename $i)">>$report
 	flat_exists=""
-	# Check each subfolder
+	# Check each subitem, if it's a folder...
 	for j in $(basename $i)/*; do
+		  if [[ -d $j ]]; then
 		  # Report if you find a flatfield, else process Volume and Page info
 		  if [[ $j =~ FLATS_* ]]; then
 			  echo "     Flatfield folder found: $j">>$report
@@ -62,8 +65,10 @@ for i in */; do
 			  if [[ "$vol_exists" != "true" ]]; then all_vol_names+=("$vol_name"); declare -a $vol_name; fi
 			  vol_exists=""
 			  
+			  # Fancy string substitution
 			  TEMP="\${$vol_name[@]}"
 			  LIST=`eval echo $TEMP`
+			  # See if this page already exists in our folio list. If it does, call a warning. NOTE: This can probably be updated with the compare function
 			  for number in $LIST; do
 			  	if [[ "$number" == "$page_name" ]]; then
 			  		duplicate="true"
@@ -76,13 +81,16 @@ for i in */; do
 			  fi
 			  duplicate=""
 		  fi
+		  fi
 	done	  
+	# Call a warning if there isn't a flatfield folder in the daily folder
 	if [[ "$flat_exists" != "true" ]]; then
 		echo "     WARNING: Flatfield folder not detected for $(basename $i)!">>$report
 	fi
 	echo >>$report
 done
 
+# List the folios and corresponding pages discovered in the previous process
 echo "----------------------------">>$report
 echo "Summary of Volumes and Pages">>$report
 echo "----------------------------">>$report
@@ -115,30 +123,32 @@ echo "          $LIST">>$report
 	for j in $(basename $i)/*; do
 	echo >>$report
 		if [[ "$(basename $j)" != FLATS_* ]]; then
+		# If there's a processed folder in this subfolder...
 		if [[ -d $(basename $i)/$(basename $j)/Processed ]]; then
+		# Create an array for this subfolder that contains all the discovered wavelengths
 		for k in $(basename $i)/$(basename $j)/Processed/*.tif; do
 			test_array+=("$(exiv2 -qpa $k | grep Exif.Photo.SpectralSensitivity | awk '{print $4}' | sed 's/(\([0-9A-Za-z]*\)nm,/\1/')")
 			test_array=( $(for m in ${test_array[@]}; do echo "$m "; done | sort | tr -d '\n') )
 		done
-		
+		# Report this array		
 		echo "     Wavelengths for $(basename $j):">>$report
 		echo "          ${test_array[@]}">>$report
 		echo >>$report
-		
+		# Compare the array against the "Standard" wavelengths
 		for k in ${MASTER[@]}; do
 			containsElement "$k" "${test_array[@]}"
 			if [[ $? == "1" ]]; then
 				echo "     WARNING: $(basename $j) in daily folder $(basename $i) is missing Standard Eureka Vision exposure ${k}nm">>$report
 			fi
 		done
-		
+		# Compare the array against the discovered flatfield exposures to see if we're missing an exposure that was in the flatfields
 		for k in $LIST; do
 			containsElement "$k" "${test_array[@]}"
 			if [[ $? == "1" ]]; then
 				echo "     WARNING: $(basename $j) in daily folder $(basename $i) is missing exposure ${k}nm found in the flatfields folder">>$report
 			fi
 		done
-		
+		# Compare against the discovered flatfields to see if an exposure is missing a corresponding flatfield
 		for k in ${test_array[@]}; do
 			containsElement "$k" $LIST
 			if [[ $? == "1" ]]; then
@@ -148,6 +158,7 @@ echo "          $LIST">>$report
 		
 		unset test_array
 		else
+			# Call a warning if there isn't a Processed folder
 			echo "     WARNING: Processed folder not found in $j">>$report
 			echo >>$report
 		fi
@@ -155,6 +166,7 @@ echo "          $LIST">>$report
 	done
 done
 
+# Read back any warnings in the report to the console
 cat "$report" | grep "WARNING"
 echo
 echo Report Generated to $report
