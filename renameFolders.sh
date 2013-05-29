26eb1090442bc010132cab32e0e25d8ab2844084
Seth Parker <csparker247@gmail.com>
2013-05-29 08:49:15 -0400

    Simple folder rename script for use in daily folders.

---
+++ renameFolders.sh
@@ -0,0 +1,7 @@
+for i in */; do
+	if [[ "$(basename $i)" != FLATS_* ]]; then
+		volume=$(basename $i | sed 's/\([A-Za-z]*\)[0-9]*[A-Za-z]*/\1/')
+		page=$(basename $i | sed 's/[A-Za-z]*\([0-9]*[A-Za-z]*\)/\1/')
+		mv $(basename $i) $volume-$page
+	fi
+done
