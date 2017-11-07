983cf8ec7bc1c406b6b5b995d1b46cc97f325e4c
Seth Parker <csparker247@gmail.com>
2017-11-07 17:07:01 -0500

    Update the flatfield and despot apps for C++11 and OpenCV3

--- flatfield/despot.cpp
+++ flatfield/despot.cpp
@@ -1,9 +1,10 @@
 #include <climits>
-#include <stdio.h>
 #include <string>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/photo/photo.hpp>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
@@ -15,65 +16,73 @@ int main( int argc, char *argv[] )
     fprintf( stderr, "Usage: %s image_file out_file\n", argv[0] );
   }
 
-//	Gets arguments from CLI
-	const char *img_file = argv[1];
-	const char *out_file = argv[2];
+  //  Gets arguments from CLI
+  const char* img_file = argv[1];
+  const char* out_file = argv[2];
 
-//	Sets up matrices and variables		
-	Mat image;
-	Mat workingimage;
-	Mat lowdepth;
-	Mat mask;
-	Mat invertmask;
-	double minVal, maxVal;
-	
-//	Load image file	
-	image = imread( img_file, CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH );	
+  //  Sets up matrices and variables
+  Mat image;
+  Mat workingimage;
+  Mat lowdepth;
+  Mat mask;
+  Mat invertmask;
+  double minVal, maxVal;
 
-//	Get information about image and convert to float working image
-	image.convertTo( workingimage, CV_32F );
-	int lastcol = workingimage.cols - 1;
-	int lastrow = workingimage.rows - 1;
-	minMaxLoc(workingimage, &minVal, &maxVal);
+  //  Load image file
+  image = imread(img_file, CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH);
 
-//	Points that comprise vector of 1px-wide image border	
-	Point px0 = Point( lastcol, 0 );
-	Point p0y = Point( 0, lastrow );
-	Point pxy = Point( lastcol, lastrow);
-	
-//	Make mask by thresholding image and growing output		
-	threshold( workingimage, mask, 42000, 65535, 0 );
-	
-	// Fills border on right and bottom edges of image
-	line( mask, px0 , pxy, (maxVal, maxVal, maxVal), 1);
-	line( mask, p0y , pxy, (maxVal, maxVal, maxVal), 1);
-	
-	int erosion_size = 1;   
-	Mat element = getStructuringElement( MORPH_ELLIPSE,
-    					                 Size(2 * erosion_size + 1, 2 * erosion_size + 1), 
-                      					 Point(erosion_size, erosion_size) );
-	erode ( mask, mask, element );
+  //  Get information about image and convert to float working image
+  image.convertTo(workingimage, CV_32F);
+  int lastcol = workingimage.cols - 1;
+  int lastrow = workingimage.rows - 1;
+  minMaxLoc(workingimage, &minVal, &maxVal);
 
-//	Setup for inpainting
-	mask.convertTo( mask, CV_8U );
-	bitwise_not( mask, invertmask );
-	workingimage.convertTo(lowdepth, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
-	
-//	Perform inpainting and blur (currently disabled)
-	inpaint(lowdepth, invertmask, lowdepth, 8.0, 1);
-	// GaussianBlur( lowdepth, lowdepth, Size( 21, 21 ), 0, 0 );
+  //  Points that comprise vector of 1px-wide image border
+  Point px0 = Point(lastcol, 0);
+  Point p0y = Point(0, lastrow);
+  Point pxy = Point(lastcol, lastrow);
 
-//	Convert working images back to 16-bit unsigned
-	minMaxLoc(lowdepth, &minVal, &maxVal);
-	lowdepth.convertTo( lowdepth, CV_16U, 65535.0/(maxVal - minVal), -minVal * 65535.0/(maxVal - minVal));
-	mask.convertTo( mask, CV_16U, 65535.0/(maxVal - minVal), -minVal * 65535.0/(maxVal - minVal));
-	invertmask.convertTo( invertmask, CV_16U, 65535.0/(maxVal - minVal), -minVal * 65535.0/(maxVal - minVal));
+  //  Make mask by thresholding image and growing output
+  threshold(workingimage, mask, 42000, 65535, 0);
 
-//	Subtract bad pixels from original image and add new pixels (limits data loss)
-	workingimage = (image - invertmask) + (lowdepth - mask);
+  // Fills border on right and bottom edges of image
+  line(mask, px0, pxy, {maxVal, maxVal, maxVal}, 1);
+  line(mask, p0y, pxy, {maxVal, maxVal, maxVal}, 1);
 
-//	Write-out image	
-	imwrite( out_file, workingimage );
-	
+  int erosion_size = 1;
+  Mat element = getStructuringElement(
+      MORPH_ELLIPSE, Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+      Point(erosion_size, erosion_size));
+  erode(mask, mask, element);
+
+  //  Setup for inpainting
+  mask.convertTo(mask, CV_8U);
+  bitwise_not(mask, invertmask);
+  workingimage.convertTo(
+      lowdepth, CV_8U, 255.0 / (maxVal - minVal),
+      -minVal * 255.0 / (maxVal - minVal));
+
+  //  Perform inpainting and blur (currently disabled)
+  inpaint(lowdepth, invertmask, lowdepth, 8.0, 1);
+  // GaussianBlur( lowdepth, lowdepth, Size( 21, 21 ), 0, 0 );
+
+  //  Convert working images back to 16-bit unsigned
+  minMaxLoc(lowdepth, &minVal, &maxVal);
+  lowdepth.convertTo(
+      lowdepth, CV_16U, 65535.0 / (maxVal - minVal),
+      -minVal * 65535.0 / (maxVal - minVal));
+  mask.convertTo(
+      mask, CV_16U, 65535.0 / (maxVal - minVal),
+      -minVal * 65535.0 / (maxVal - minVal));
+  invertmask.convertTo(
+      invertmask, CV_16U, 65535.0 / (maxVal - minVal),
+      -minVal * 65535.0 / (maxVal - minVal));
+
+  //  Subtract bad pixels from original image and add new pixels
+  workingimage = (image - invertmask) + (lowdepth - mask);
+
+  //  Write-out image
+  imwrite(out_file, workingimage);
+
   return 0;
 }
--- flatfield/pngflatten.cpp
+++ flatfield/pngflatten.cpp
@@ -1,33 +1,31 @@
 #include <climits>
-#include <stdio.h>
 #include <string>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
-
-using namespace cv;
-using namespace std;
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <stdio.h>
 
 int main( int argc, char *argv[] )
 {
-  if( argc < 4 )
-  {
-    fprintf( stderr, "Usage: %s flat_file image_file out_file\n", argv[0] );
-  }
-	const char *flat_file = argv[1];
-	const char *img_file = argv[2];
-	const char *out_file = argv[3];
-	
-	Mat flat_img;
-	flat_img = imread( flat_file, CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH );	
-	Mat image;
-	image = imread( img_file, CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH );
-		
-	GaussianBlur( flat_img, flat_img, cv::Size(3,3), 0, 0, BORDER_REPLICATE );
-	divide( image, flat_img, image, image.depth() == 0 ? UCHAR_MAX : USHRT_MAX );
-	vector<int> compression_params;
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s flat_file image_file out_file\n", argv[0]);
+    }
+    auto flat_file = argv[1];
+    auto img_file = argv[2];
+    auto out_file = argv[3];
+
+    auto flat_img = cv::imread(flat_file, -1);
+    auto image = cv::imread(img_file, -1);
+
+    cv::GaussianBlur(
+        flat_img, flat_img, cv::Size(3, 3), 0, 0, cv::BORDER_REPLICATE);
+    cv::divide(
+        image, flat_img, image, image.depth() == 0 ? UCHAR_MAX : USHRT_MAX);
+    std::vector<int> compression_params;
     compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
     compression_params.push_back(9);
-	imwrite( out_file, image, compression_params );
-	
-  return 0;
+    cv::imwrite(out_file, image, compression_params);
+
+    return EXIT_SUCCESS;
 }
